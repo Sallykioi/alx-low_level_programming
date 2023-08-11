a7ee2ab4adec3eda81e5d657e4e09dec2f3f4a93
Sallykioi <kioisally@gmail.com>
2023-08-11 02:32:59 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints char and int
+ * Description - Using fput to print characters and numbers
+ * Return: 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stderr);
+return (1);
+}
