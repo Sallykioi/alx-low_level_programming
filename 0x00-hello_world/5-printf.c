d91d71b68bbf0c3a50f3dce3c90400e4a5e7be80
Sallykioi <kioisally@gmail.com>
2023-08-11 00:09:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses printf
+ * Description - A program that uses printf function to print a new line
+ * Return: comment
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
