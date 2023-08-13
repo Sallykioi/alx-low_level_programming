48b3124011b3cd9890799d9b2981d9ae3c378d67
Sallykioi <kioisally@gmail.com>
2023-08-13 11:17:25 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Function
+ *
+ * Description: A program that digits of base 10 from 0
+ *
+ * Return: Comment
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar (i + '0');
+	putchar ('\n');
+	return (0);
+}
