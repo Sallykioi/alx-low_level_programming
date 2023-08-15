16f101e30ba683d48c4b585526c213944da5ceba
Sallykioi <kioisally@gmail.com>
2023-08-15 13:01:12 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Function that checks alphabet character
+ * @c: Checking parameter
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
