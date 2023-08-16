c5d3c683c742a3cc9a4d1e3b3b03290bf04a163f
Sallykioi <kioisally@gmail.com>
2023-08-16 05:30:02 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - A unction that adds two numbers and returns the value
+ *Description: Sums two numbers and returns the value
+ *@a: Parameter 1
+ *@b: Parameter 2
+ *Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
