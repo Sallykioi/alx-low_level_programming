16bceed4798e0aa12e882758d27fc28fd43e53bd
Sallykioi <kioisally@gmail.com>
2023-08-20 02:51:58 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,16 +8,8 @@
 
 void print_square(int size)
 {
-	if (size <= 0)
-		_putchar('\n');
-	else
+	int i, j;
+
+	for (i = 0 ; i <= 10 ; i++)
 	{
-		int i,j;
-		for (i = 0 ; i = size ; i++)
-			_putchar('#');
-		for (j = 0 ; j = size ; j++)
-			_putchar('#');
-		_putchar('\n');
-	}
-}
-		
+		for (j = 0 ; j <= 
