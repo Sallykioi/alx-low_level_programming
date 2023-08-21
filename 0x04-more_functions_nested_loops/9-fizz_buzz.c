78a35cc3fb3165f94a11eaa9e108533e514c5bc6
Sallykioi <kioisally@gmail.com>
2023-08-21 01:29:47 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers from 1 to 100 and replaces fizz with numbers 
+ divisible by 3 and buzz with numbers divisible by 5
+ *@i:Parameter 1
+ * Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
