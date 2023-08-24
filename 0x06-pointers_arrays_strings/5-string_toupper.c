dfd671c03e1d3b075539a08e64233187c7c049df
Sallykioi <kioisally@gmail.com>
2023-08-24 13:43:48 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - function that changes all lowercase letters of a
+ * string to uppercase
+ *@str:Parameter name
+ * Return: Always 0
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 61 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
